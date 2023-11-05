c0f3e31b4392895c48cd67b6811b1b4f299f9970
Kishore Kumar v <kishore.kumarkv93@gmail.com>
2023-11-05 11:33:11 +0530

    Final commit

--- src/DimondPricePrediction/components/data_ingestion.py
+++ src/DimondPricePrediction/components/data_ingestion.py
@@ -43,6 +43,11 @@ class DataIngestion:
             logging.info("Saved Train and Test data in artifacts folder")
             logging.info("train test split completed")
 
+            return (
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path
+            )
+
         except Exception as e:
             logging.info("exception occured during the DataIngestion stage")
             raise customexception(e,sys)
